57130dd9c8d0140d9d5e888e504bed3431517940
Eteri <147832143+eterijafarova@users.noreply.github.com>
2025-06-27 13:26:30 +0400

    Update index.tsx

--- app/users/index.tsx
+++ app/users/index.tsx
@@ -1,33 +1,32 @@
 import axios from "axios";
 import { Link } from "expo-router";
 import { useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import { Text, View, FlatList } from "react-native";
 import { Users } from "@/src/types/users";
 
 const UsersScreen = () => {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<Users[]>([]);
 
   useEffect(() => {
     const getUsers = async () => {
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/users?limit=5"
-      );
-
-      setUsers(response.data.products);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+      setUsers(res.data);
     };
     getUsers();
   }, []);
 
   return (
-    <View>
-      <Text>Users</Text>
-      {users.map(({ id, name }) => {
-        return (
-          <Link key={id} href={`users/${id}`}>
-            {name}
+    <View style={{ padding: 16 }}>
+      <Text style={{ fontSize: 20, marginBottom: 10 }}>Users</Text>
+      <FlatList
+        data={users}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <Link href={`/users/${item.id}`}>
+            <Text style={{ marginVertical: 4 }}>{item.name}</Text>
           </Link>
-        );
-      })}
+        )}
+      />
     </View>
   );
 };
